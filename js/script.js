1f469d3122bdb21e5abe90191d8be5af28eac876
Maksim <hasuteam@mail.com>
2021-04-12 17:23:39 +0300

    12 урок первый скрипт

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-'use strict';
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
 
-let incr = 10,
-    decr = 10;
-// incr++;
-// decr--;
-console.log(incr++);
-console.log(decr--);   
-console.log(incr);
-console.log(++incr);
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let firstQuestion = prompt("Один из последних просмотренных фильмов?",''),
+    secondQuestion = prompt("На сколько его оцените?",''),
+    thridQuestion = prompt("Один из последних просмотренных фильмов?",''),
+    fourthQuestion = prompt("На сколько его оцените?",'');
+
+personalMovieDB.movies[firstQuestion] =  secondQuestion;   
+personalMovieDB.movies[thridQuestion] =  fourthQuestion;   
+console.log(personalMovieDB);
