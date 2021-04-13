a45b0af632ccba1e893765e5f48f53584f20e5d6
Maksim <hasuteam@mail.com>
2021-04-13 20:08:03 +0300

    24 урок отредактировал код

--- js/script.js
+++ js/script.js
@@ -79,56 +79,6 @@ let personalMovieDB = {
     }   
 };
 
-personalMovieDB.toggleVisibleMyDB();
-personalMovieDB.showMyDb();
-console.log(personalMovieDB.privat);
-// function rememberMyFilms () {
-//     for (let i = 0; i < 2; i++){
-//         let a = prompt("Один из последних просмотренных фильмов?",''),
-//             b = prompt("На сколько его оцените?",'');
-        
-//         if(a != null && b != null && a != '' && b != '' && a.length < 50){
-//             personalMovieDB.movies[a] = b;
-//         }else{
-//             console.log('error');
-//             i--;
-//         }
-          
-//     }
-// }
-// rememberMyFilms();
-
-// function detectPersonalLevel () {
-//     if(personalMovieDB.count < 10){
-//         console.log("Просмотренно довольно мало фильмов");
-//     }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-//         console.log("Вы класический зритель");
-//     }else if (personalMovieDB.count >= 30){
-//         console.log("Вы киноман");
-//     }else {
-//         console.log("Произошла ошибка!");
-//     }     
-//     console.log(personalMovieDB);
-    
-// } 
-// detectPersonalLevel();
-
-//  function showMyDb() {
-//      if(personalMovieDB.privat == false){
-//          console.log(personalMovieDB);
-//      }
-
-//  }
-//  showMyDb();
-
-
-
-function writeYourGenres () {
-    for ( let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i-1]= prompt(`Ваш любимый жанр 
-        под номером  ${i}`,'');
-    } 
-}  
 
 
 
